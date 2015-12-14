621a7f652d206b1910cfcda22125ba50ed4c048c
sedrubal <sebastian.endres@online.de>
2015-12-14 04:18:01 +0100

    Improved sticky navbar

--- js/faufablab.js
+++ js/faufablab.js
@@ -16,18 +16,22 @@ $("a[href^='#'].smooth_scroll").on('click', function(e) {
 
 /* STICKY NAVBAR
 ------------------- */
-if (undefined != $('.navbar-sticky').offset()) {
-	navbartop = $('.navbar-sticky').offset().top;               
-	scrollIntervalID = setInterval(stickIt, 10);
-}
-
-function stickIt() {
-
-	var navbar = $('.navbar-sticky');
-
-	if ($(window).scrollTop() >= (navbartop)) {
-		navbar.addClass("navbar-fixed-top")
-	} else {
-		navbar.removeClass("navbar-fixed-top")
+if (undefined != $('.navbar-sticky')) {
+	function stickynavbar() {
+		if ($(window).scrollTop() >= (navbartop)) {
+			navbarsticky.addClass("navbar-fixed-top");
+			navbarplaceholder.show();
+		} else {
+			navbarsticky.removeClass("navbar-fixed-top");
+			navbarplaceholder.hide();
+		}
 	}
+
+	var navbarsticky = $('.navbar-sticky');
+	navbartop = navbarsticky.offset().top;
+	var navbarplaceholder = navbarsticky.clone().insertAfter(navbarsticky).addClass('navbar-static-top');
+	navbarplaceholder[0].style.opacity=0;
+	stickynavbar();
+
+	$(document).scroll(stickynavbar);
 }
